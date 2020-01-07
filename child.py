354b0fb97973bfef40c55a2c75b27b3a24df1812
eugene <utyfdfrthby08@gmail.com>
2020-01-07 21:58:53 +0200

    from pc home

---
+++ child.py
@@ -0,0 +1,16 @@
+from hello import Calculation
+
+
+class Child(Calculation):
+    num2 = 500
+
+    def __init__(self):
+        Calculation.__init__(self)
+
+    def test2(self, a, b):
+        return print(a + b)
+
+
+obj3 = Child()
+obj3.test2(5, 10)
+obj3.test5()
--- hello.py
+++ hello.py
@@ -1,1 +1,14 @@
-print("Hello")
+class Calculation:
+    num = 100
+    width = 0
+    length = 0
+
+    def __init__(self):
+        print("Object is created")
+
+    def test(self, a, b):
+        return print(a + b)
+
+    def test5(self):
+        return print("From Parent")
+
